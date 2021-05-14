f98d46c48c1cb89a248e58085faec167dff2f03d
JeffersonQin <1247006353@qq.com>
2021-05-14 20:27:30 +0800

    [Update] upper, lower case sensitive

--- lib/utils.py
+++ lib/utils.py
@@ -69,5 +69,5 @@ def slugify(value, allow_unicode=True):
 		value = unicodedata.normalize('NFKC', value)
 	else:
 		value = unicodedata.normalize('NFKD', value).encode('ascii', 'ignore').decode('ascii')
-	value = re.sub(r'[^\w\s-]', '', value.lower())
+	value = re.sub(r'[^\w\s-]', '', value)
 	return re.sub(r'[-\s]+', '-', value).strip('-_')
