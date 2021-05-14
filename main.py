7ec48a34e12b0fcf59d435d0d2d8f0030463702e
JeffersonQin <1247006353@qq.com>
2021-05-14 11:13:09 +0800

    [Add] slug/download/write-contents modules

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ import requests
 import git
 import sys
 import shutil
+import time
+import re
+import unicodedata
+import json
+
 
 # initialize paths & caches
 root_dir = os.path.split(os.path.abspath(__file__))[0]
@@ -49,29 +54,100 @@ def csuccess(message: str):
 
 #### Fetching Start ####
 
-def fetch_contents():
+def fetch_resource(resource: str):
+	global cmd_name
+	cmd_name = sys._getframe().f_code.co_name
+
+	clog(f'fetching {resource}')
+
+	cache_dir = os.path.join(root_dir, 'cache/')
+	file_dir = os.path.join(cache_dir, f'{resource}.json')
+
+	if not os.path.exists(cache_dir): os.mkdir(cache_dir)
+
+	download_file(f"{conf['remote']['url']}/fetch_{resource}?token={conf['remote']['token']}", file_dir)
+	
+	try:
+		with open(file_dir, 'r') as f:
+			res = json.load(f)
+			clog(f'RESPONSE: {res}')
+			if res['code'] == 1: 
+				csuccess(f'connectivity test passed, message: {res["message"]}')
+				return res['data']
+			else: 
+				cerr(f'fetch contents failed, message: {res["message"]}')
+				return None		
+	except Exception as e:
+		cerr(f'error occurred: {repr(e)}')
+	
+#### Fetching End ####
+
+#### utilities start ####
+
+def download_file(url, dir):
+	global cmd_name
+	cmd_name = sys._getframe().f_code.co_name
+
+	clog(f'start downloading: {url} => {dir}')
+	try:
+		headers = {'Proxy-Connection':'keep-alive'}
+		r = requests.get(url, stream=True, headers=headers)
+		length = float(r.headers['content-length'])
+		f = open(dir, 'wb+')
+		# download size
+		count = 0
+		with click.progressbar(label="Downloading from remote: ", length=length) as bar:
+			for chunk in r.iter_content(chunk_size = 512):
+				if chunk:
+					f.write(chunk)
+					bar.update(len(chunk))
+		f.close()
+	except Exception as err:
+		cerr(f'error: {repr(err)}')
+
+def slugify(value, allow_unicode=True):
+    """
+	Taken and modified from django/utils/text.py
+	Copyright (c) Django Software Foundation and individual contributors.
+	All rights reserved.
+    Convert to ASCII if 'allow_unicode' is False. Convert spaces or repeated
+    dashes to single dashes. Remove characters that aren't alphanumerics,
+    underscores, or hyphens. Convert to lowercase. Also strip leading and
+    trailing whitespace, dashes, and underscores.
+    """
+    value = str(value)
+    if allow_unicode:
+        value = unicodedata.normalize('NFKC', value)
+    else:
+        value = unicodedata.normalize('NFKD', value).encode('ascii', 'ignore').decode('ascii')
+    value = re.sub(r'[^\w\s-]', '', value.lower())
+    return re.sub(r'[-\s]+', '-', value).strip('-_')
+
+#### utilities end ####
+
+#### localize start ####
+
+def write_contents(data: list):
 	global cmd_name
 	cmd_name = sys._getframe().f_code.co_name
 	
-	clog('fetching contents...')
+	clog('writing contents...')
 	try:
-		response = requests.get(f"{conf['remote']['url']}/fetch_contents?token={conf['remote']['token']}")
-		# If the response was successful, no Exception will be raised
-		response.raise_for_status()
-	except HTTPError as http_err:
-		cerr(f'HTTP error occurred: {repr(http_err)}')
-	except Exception as err:
-		cerr(f'other error occurred: {repr(err)}')
-	else:
-		res = response.json()
-		clog(f'RESPONSE: {res}')
-		if res['code'] == 1: csuccess('connectivity test passed')
-		else: 
-			cerr(f'fetch contents failed, message: {res["message"]}')
-			return
-		git_branch()
+		if not os.path.exists(os.path.join(wp_dir, './posts')):
+			os.mkdir(os.path.join(wp_dir, './posts'))
+		if not os.path.exists(os.path.join(wp_dir, './pages')):
+			os.mkdir(os.path.join(wp_dir, './pages'))
+		for item in data:
+			type = f"{item['type']}s"
+			create_local_time = time.localtime(item['created'])
+			create_year = create_local_time.tm_year
+			create_month = create_local_time.tm_mon
+			title = slugify(item['title'])
+			clog(f'{title}, {create_year}, {create_month}, {type}')
+	except Exception as e:
+		cerr(f'error: {repr(e)}')
 
-#### Fetching End ####
+#### localize end ####
 
 #### local git start ####
 
@@ -89,6 +165,10 @@ def git_branch_create(branch: str):
 def git_checkout(branch: str):
 	wp_git = git_repo().git
 	return wp_git.checkout(branch)
+
+def git_status_native():
+	wp_git = git_repo().git
+	return wp_git.status()
 
 #### Local git end ####
 
@@ -115,6 +195,7 @@ def init():
 	try:
 		git.Repo.init(path=wp_dir)
 		csuccess('git initialization success.')
+		# TODO: write file structure
 		with open(readme_dir, 'w+') as f:
 			f.write('This file is created automatically by [typexo-cli](https://github.com/JeffersonQin/typexo-cli)')
 		csuccess('write file test success.')
@@ -142,7 +223,7 @@ def rm():
 	clog('triggered to delete')
 	click.echo('IMPORTANT: this command will delete everything in the workplace folder. Are you sure to continue? [y/n] ', nl=False)
 	user_in = input()
-	if (user_in != 'y' and user_in != 'Y'): return;
+	if (user_in != 'y' and user_in != 'Y'): return
 	
 	try:
 		shutil.rmtree(wp_dir)
@@ -172,9 +253,13 @@ def pull():
 	
 	clog('pulling from PROD environment...')
 	try:
-		# PREREQUISITE: changes are staged, master branch clean
-		
-		# PREREQUISITE: make sure that `prod` branch does not exist 
+		# PREREQUISITE: changes are staged, current branch clean
+		status_res = git_status_native()
+		clog(f'git status: \n{status_res}')
+		if not status_res.split('\n')[1] == 'nothing to commit, working tree clean':
+			cerr('working tree not clean, make sure that all the changes are staged and committed.')
+			return
+		# PREREQUISITE: make sure that `prod` branch does not exist
 		# get initial branches
 		branch_res = git_branch()
 		clog(f'checking branches: \n{branch_res}')
@@ -296,7 +381,11 @@ def prod_test():
 
 @cli.command()
 def test():
-	git_branch_create('prod')
+	data = fetch_resource('contents')
+	if data is None:
+		cerr('DATA FETCHING FAILED, exiting program.')
+		return
+	write_contents(data)
 
 #### Command Line Interface (CLI) End ####
 
