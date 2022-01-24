7be19e4ebe86e521965ecd159f78e914b443bac3
gyro永不抽风 <1247006353@qq.com>
2022-01-24 16:00:32 +0800

    feat: update echo lib

--- lib/echo.py
+++ lib/echo.py
@@ -23,21 +23,49 @@ def get_subroutine():
 	return subroutine_stack[-1]
 
 
-def clog(message: str):
+def _clog(message: str):
 	click.echo(click.style(f"[{get_subroutine()}]", bg='magenta', fg='white'), nl=False)
-	click.echo(f" {message}")
+	click.echo(message)
 
 
-def cerr(message: str):
+def clog(*messages):
+	val = ''
+	for message in messages:
+		val = val + ' ' + str(message)
+	_clog(val)
+
+
+def _cerr(message: str):
 	click.echo(click.style(f"[{get_subroutine()}]", bg='magenta', fg='white'), nl=False)
 	click.echo(click.style(f" {message}", fg = 'bright_red'))
 
 
-def csuccess(message: str):
+def cerr(*messages):
+	val = ''
+	for message in messages:
+		val = val + ' ' + str(message)
+	_cerr(val)
+
+
+def _csuccess(message: str):
 	click.echo(click.style(f"[{get_subroutine()}]", bg='magenta', fg='white'), nl=False)
 	click.echo(click.style(f" {message}", fg = 'green'))
 
 
-def cexit(message: str):
+def csuccess(*messages):
+	val = ''
+	for message in messages:
+		val = val + ' ' + str(message)
+	_csuccess(val)
+
+
+def _cexit(message: str):
 	cerr(f'{message}, exiting program.')
 	sys.exit(1)
+
+
+def cexit(*messages):
+	val = ''
+	for message in messages:
+		val = val + ' ' + str(message)
+	_cexit(val)
